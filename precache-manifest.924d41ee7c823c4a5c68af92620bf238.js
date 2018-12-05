f30bad8a392315fb7f59e4315c0b69bb99e1afc5
Wojciech Baczyński <wp.baczynski@gmail.com>
2018-12-05 10:23:21 +0100

    Updates

---
+++ precache-manifest.924d41ee7c823c4a5c68af92620bf238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5a98745ef385c3ddf9a",
+    "url": "/TicTacToe/static/css/main.d221df2d.chunk.css"
+  },
+  {
+    "revision": "e5a98745ef385c3ddf9a",
+    "url": "/TicTacToe/static/js/main.e5a98745.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/TicTacToe/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d30c1851cd5f3a287cfa6f4ebca76f6b",
+    "url": "/TicTacToe/index.html"
+  }
+];
